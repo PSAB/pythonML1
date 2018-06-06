d52bc9382a8c42844f2e13e7e0903a6c5c72def6
Pavan S Sabnis <pvnsbns@gmail.com>
2018-06-06 16:54:07 -0700

    Fill in missing values in Salary column in data from Data.csv

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -12,6 +12,10 @@ dataset = read.csv('Data.csv')
 dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), dataset$Age)
   # if there was a missing value, it was replaced w/ col average
 
+# Do the same thing for salary column
+dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), dataset$Salary)
+
+
 
 
 
