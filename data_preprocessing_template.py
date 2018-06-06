98752153cc4e128c5404a82013a9fd3e42a485aa
Pavan S Sabnis <pvnsbns@gmail.com>
2018-06-06 16:05:05 -0700

    create Imputer object to replace missing csv data and fit onto dataset X

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -15,10 +15,17 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
+print(X)
+
 
 # taking care of the missing data
 from sklearn.preprocessing import Imputer
+# Specifying our strategy for imputing missing values
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+# Fitting this Imputer variable onto out matrix data X
+imputer = imputer.fit(X[:, 1:3]) # Specify columns that contain the missing data
+# Replace the missing data with the values
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 
 
