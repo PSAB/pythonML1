d64b32cc31499a5340635603e9a7c36d86b31753
Pavan S Sabnis <pvnsbns@gmail.com>
2018-09-02 16:12:34 -0700

    remove code relating to imputer and categorical encoding to cfeate data preprocessing template

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -17,41 +17,6 @@ Y = dataset.iloc[:, 3].values
 
 
 
-# taking care of the missing data
-from sklearn.preprocessing import Imputer
-# Specifying our strategy for imputing missing values
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-# Fitting this Imputer variable onto out matrix data X
-imputer = imputer.fit(X[:, 1:3]) # Specify columns that contain the missing data
-# Replace the missing data with the values
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-
-
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# creating an object of the class we imported
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-# France is 0, Germany is 1, Spain is 2
-
-# Machine learning models are based in equations, so it's good to
-#encode categorical data into numerical values
-
-# However, there is no relational order between the countries
-
-# To prevent the ML model from interpreting order, use dummy 
-#encoding table
-
-# Fit the OneHotEncoder onto the first column of index 0
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-
-# Create the labelEncoder object for the dependent purchased column
-labelEncoder_Y = LabelEncoder()
-Y = labelEncoder_Y.fit_transform(Y)
-# Yes is 1, no is 0
-
-
 # Splitting the dataset into the training set and the test set
 from sklearn.model_selection import train_test_split
 # Immediately define the variables required for the training and testing
